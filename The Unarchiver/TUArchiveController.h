0b329e025c40a2c111f2a2a90072c6820c8cdd06
Dag ?gren <paracelsus@gmail.com>
2012-06-19 18:38:09 +0300

    Import and adapt PDF parsing code from Xee, add skeleton XAD parser

---
+++ The Unarchiver/TUArchiveController.h
@@ -0,0 +1,50 @@
+#import <XADMaster/XADSimpleUnarchiver.h>
+
+#import "TUArchiveTaskView.h"
+
+
+@class TUController,TUEncodingPopUp;
+
+@interface TUArchiveController:NSObject
+{
+	TUArchiveTaskView *view;
+	XADSimpleUnarchiver *unarchiver;
+	NSString *archivename,*destination,*tmpdest;
+	NSStringEncoding selected_encoding;
+
+	id finishtarget;
+	SEL finishselector;
+
+	BOOL cancelled,ignoreall,haderrors;
+}
+
++(void)clearGlobalPassword;
+
+-(id)initWithFilename:(NSString *)filename taskView:(TUArchiveTaskView *)taskview;
+-(void)dealloc;
+
+-(BOOL)isCancelled;
+-(NSString *)filename;
+-(NSArray *)allFilenames;
+-(BOOL)caresAboutPasswordEncoding;
+-(TUArchiveTaskView *)taskView;
+
+-(void)setIsCancelled:(BOOL)iscancelled;
+-(void)setDestination:(NSString *)destination;
+
+-(NSString *)currentArchiveName;
+-(NSString *)localizedDescriptionOfError:(XADError)error;
+-(NSString *)stringForXADPath:(XADPath *)path;
+
+-(void)runWithFinishAction:(SEL)selector target:(id)target;
+
+-(void)extractThreadEntry;
+-(void)extract;
+-(void)extractFinished;
+-(void)extractFailed;
+-(void)rememberTempDirectory:(NSString *)tmpdir;
+-(void)forgetTempDirectory:(NSString *)tmpdir;
+
+-(void)archiveTaskViewCancelled:(TUArchiveTaskView *)taskview;
+
+@end
